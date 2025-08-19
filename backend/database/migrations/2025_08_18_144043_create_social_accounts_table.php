a6cbd5a8b886152b70ff6d1722e6bf2420baaf45
marumo333 <seito5026junior@icloud.com>
2025-08-19 21:33:09 +0900

    add:userテーブルを追加

--- backend/database/migrations/2025_08_18_144043_create_social_accounts_table.php
+++ backend/database/migrations/2025_08_18_144043_create_social_accounts_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('social_accounts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('provider_name'); // google, github, x など
+            $table->string('provider_id');   // プロバイダー側のID
+            $table->string('provider_token')->nullable();
+            $table->string('provider_refresh_token')->nullable();
             $table->timestamps();
+            
+            $table->unique(['provider_name', 'provider_id']);
         });
     }
 
