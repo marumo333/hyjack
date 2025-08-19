a87d534ddb9d6cfc9d3cf2e75b29eee391bedf17
marumo333 <seito5026junior@icloud.com>
2025-08-19 21:34:41 +0900

    add:モデルファイルを追加

--- backend/app/Models/SocialAccount.php
+++ backend/app/Models/SocialAccount.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialAccount extends Model
 {
-    //
+    protected $fillable = [
+        'provider_name',
+        'provider_id',
+        'provider_token',
+        'provider_refresh_token',
+    ]
+    ;
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
