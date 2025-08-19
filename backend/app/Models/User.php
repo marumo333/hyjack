997d053eb76a92e78cf8fb86e37418c9a3735357
marumo333 <seito5026junior@icloud.com>
2025-08-19 21:36:38 +0900

    add;ソーシャルアカウントの関連を追加

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function socialAccounts(){
+        return $this->hasMany(SocialAccount::class);
+    }
 }
