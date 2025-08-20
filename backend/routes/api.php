d87334b10ada72a4a48d0392aa27f56923094b7e
marumo333 <seito5026junior@icloud.com>
2025-08-20 22:08:41 +0900

    add:apiのルート設定を追加

---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Auth\HyjackAuthController;
+use Illuminate\Support\Facades\Route;
+
+// 認証不要エンドポイント
+Route::post('/login', [HyjackAuthController::class, 'emailLogin']);
+Route::get('/social/{provider}/redirect', [HyjackAuthController::class, 'socialRedirect']);
+Route::get('/social/{provider}/callback', [HyjackAuthController::class, 'socialCallback']);
+
+// 認証必須エンドポイント
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [HyjackAuthController::class, 'logout']);
+    Route::get('/user', [HyjackAuthController::class, 'user']);
+});
