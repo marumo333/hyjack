2ff920d5006119daa89821899f65fcf4291e7426
marumo333 <seito5026junior@icloud.com>
2025-09-22 18:06:09 +0900

    fix:Kernelクラスを修正

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -1,10 +1,24 @@
 <?php
-// backend/app/Http/Kernel.php のmiddlewareに追加
 
-class Kernel
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
 {
+    /**
+     * アプリケーションのグローバルHTTPミドルウェアスタック
+     */
     protected $middleware = [
-        // ...
+        \Illuminate\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class, // Laravelコアクラスを使用
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // カスタムCORSミドルウェア
         \App\Http\Middleware\Cors::class,
     ];
+    
+    // 他のプロパティやメソッド...
 }
