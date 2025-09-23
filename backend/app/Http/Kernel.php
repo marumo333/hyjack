e70eaed2f5bdc7d539f94583e6a6e009d0457032
marumo333 <seito5026junior@icloud.com>
2025-09-23 20:50:24 +0900

    upadate:middlewareグループを追加

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\EncryptCookies;
 
 class Kernel extends HttpKernel
 {
@@ -19,6 +20,23 @@ class Kernel extends HttpKernel
         // カスタムCORSミドルウェア
         \App\Http\Middleware\Cors::class,
     ];
-    
-    // 他のプロパティやメソッド...
+
+    /**
+     * アプリケーションのHTTPミドルウェアグループ
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            // 'throttle:api',  // このコメントを外すと、レート制限が有効になります
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
 }
