1d507924ff1013ecac65c18b80c011f981da574f
marumo333 <seito5026junior@icloud.com>
2025-09-23 20:51:58 +0900

    update:Headerリストにtokenを追加

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,10 +1,20 @@
 <?php
+// config/cors.php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    
+    // このヘッダーリストにX-XSRF-TOKENを追加
+    'allowed_headers' => [
+        'X-XSRF-TOKEN',
+        'X-Requested-With', 
+        'Content-Type', 
+        'Accept', 
+        'Authorization'
+    ],
+    
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
