f3181be6755b609a31f1dea33e3b16322be73c74
marumo333 <seito5026junior@icloud.com>
2025-09-29 23:29:06 +0900

    delete:不要なルート定義を削除

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,16 +11,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [HyjackAuthController::class, 'user']);
     Route::post('logout', [HyjackAuthController::class, 'logout']);
 });
-
-//Googleログイン用のroute
-Route::get('social/google/redirect',[HyjackAuthController::class,'redirectToGoogle']);
-Route::get('social/google/callback',[HyjackAuthController::class,'handleGoogleCallback']);
-
-//Xログイン用のroute
-Route::get('social/X/redirect',[HyjackAuthController::class,'redirectToX']);
-Route::get('social/X/redirect',[HyjackAuthController::class,'handleXCallback']);
-
-//GitHubログイン用のroute
-Route::get('social/github/redirect',[HyjackAuthController::class,'redirectToGithub']);
-
-
